361eedbf68cdbe4153dc7baadc9b56da78431704
AlvaroBueno99 <55491432+AlvaroBueno99@users.noreply.github.com>
2019-10-24 19:38:22 +0200

    Añadida la impresion

--- nivel1.c
+++ nivel1.c
@@ -74,17 +74,27 @@ int check_internal(char **args){
 }
 
 int internal_cd(char **args){
+    printf("This function will change the directory.\n")
+    return 0;
+
 
 }
 
 int internal_export(char **args){
+    printf("This function will asign values to environment variables.\n")
+    return 0;
 
 }
 
 int internal_source(char **args){
+    printf("This function will execute a command line file.\n")
+    return 0;
 
 }
 
 int internal_jobs(char **args){
+    printf("This function will show the PID of the processes that are not in 
+    the foreground.\n")
+    return 0;
 
 }
