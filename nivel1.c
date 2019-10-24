a8c3b399f81625722fe2668a1f9b9c4b7f561f46
AlvaroBueno99 <55491432+AlvaroBueno99@users.noreply.github.com>
2019-10-24 19:57:50 +0200

    Update nivel1.c

--- nivel1.c
+++ nivel1.c
@@ -72,29 +72,27 @@ int parse_args(char **args, char *line){
 int check_internal(char **args){
 
 }
-
 int internal_cd(char **args){
-    printf("This function will change the directory.\n")
+    printf("This function will change the directory.\n");
     return 0;
 
 
 }
 
 int internal_export(char **args){
-    printf("This function will asign values to environment variables.\n")
+    printf("This function will asign values to environment variables.\n");
     return 0;
 
 }
 
 int internal_source(char **args){
-    printf("This function will execute a command line file.\n")
+    printf("This function will execute a command line file.\n");
     return 0;
 
 }
 
 int internal_jobs(char **args){
-    printf("This function will show the PID of the processes that are not in 
-    the foreground.\n")
+    printf("This function will show the PID of the processes that are not in the foreground.\n");
     return 0;
 
 }
