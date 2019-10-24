a4bc2b1a17e3901c61897ef67f3e70fefceef8e2
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-10-24 20:28:09 +0200

    como token solo acepta " ".

--- nivel1.c
+++ nivel1.c
@@ -8,16 +8,16 @@
 * Date: 
 */
 
-// Libreries.
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 //Constants.
 #define _POSIX_C_SOURCE 200112L
 #define COMMAND_LINE_SIZE 1024
 #define ARGS_SIZE 64
 #define PROMPT '$'
+
+// Libreries.
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 // Function headers.
 char * read_line(char *line);
@@ -34,8 +34,10 @@ int internal_jobs(char **args);
 */
 int main(){
     char *cmd = (char *) malloc (sizeof(char) * COMMAND_LINE_SIZE);
-    while(read_line(cmd)){
-
+    if (cmd){
+        while(read_line(cmd)){
+            execute_line(cmd);
+        }
     }
     return 0;
 }
@@ -61,38 +63,46 @@ char * read_line(char *line){
     }
 }
 
+/*
+ * Prepara la linea para la ejecucion y divide los tokens.
+ */
 int execute_line(char *line){
-
+    char **args = malloc (sizeof(char *) * ARGS_SIZE);
+    if (args){
+        parse_args(args,line);
+        check_internal(args);
+    }
+    free(args);
 }
 
 int parse_args(char **args, char *line){
-    
+    int i = 0;
+    char *token = strtok(line, " ");
+    args [i] = token;
+    while (token != NULL){
+        printf("%s\n", args[i]);
+        i++;
+        token = strtok(NULL, " ");
+        args[i] = token;
+    }
 }
 
 int check_internal(char **args){
 
 }
-int internal_cd(char **args){
-    printf("This function will change the directory.\n");
-    return 0;
 
+int internal_cd(char **args){
 
 }
 
 int internal_export(char **args){
-    printf("This function will asign values to environment variables.\n");
-    return 0;
 
 }
 
 int internal_source(char **args){
-    printf("This function will execute a command line file.\n");
-    return 0;
 
 }
 
 int internal_jobs(char **args){
-    printf("This function will show the PID of the processes that are not in the foreground.\n");
-    return 0;
 
 }
