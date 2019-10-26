149df7b54f2bb6f90b63f359125aca22f233c7f5
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-10-26 13:41:24 +0200

    Comentarios y resolucion de fgets().
    Añadido comentarios y arreglo al Segmentation fault 11 de fgets()

--- nivel1.c
+++ nivel1.c
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include <string.h>
 
-//Constants.
+// Constants.
 #define _POSIX_C_SOURCE 200112L
 #define COMMAND_LINE_SIZE 1024
 #define ARGS_SIZE 64
@@ -31,11 +31,17 @@ int internal_source(char **args);
 int internal_jobs(char **args);
 
 /*
-* Here starts the program.
+* Main program, here starts the execution.
 */
 int main(){
+    
+    // Allocates memory for the input command line.
     char *cmd = (char *) malloc (sizeof(char) * COMMAND_LINE_SIZE);
+    
+    // If there is enough memory for cmd then execute the loop.
     if (cmd){
+        
+        // Read line and execute it.
         while(read_line(cmd)){
                  execute_line(cmd);
         }
@@ -46,48 +52,92 @@ int main(){
 /*
 * Function: read_line:
 * --------------------
-* Prints the prompt and reads the input introduced by stdin.
+* Prints the prompt and reads the input introduced in stdin.
 *
 *  line: pointer where will be stored the input introduced by stdin.
 *
 *  returns: pointer of input introduced.
 */
 char * read_line(char *line){
+    
+    // Allocates memory for the prompt and check if it has been able to do it.
     char *prompt = malloc (sizeof(char) * COMMAND_LINE_SIZE);
     if (prompt)
     {
+        // Gets the current work directory.
         getcwd(prompt,COMMAND_LINE_SIZE);
+        
+        // Prints the prompt and the separator.
         printf ("%s %c ",prompt, PROMPT);
+        
+        // Reads input introduced in stdin by the user.
         fgets(line, COMMAND_LINE_SIZE, stdin);
+        
+        // frees the memory for prompt.
         free(prompt);
-        return line;
     }
+    return line;
 }
 
 /*
- * Prepara la linea para la ejecucion y divide los tokens.
- */
+* Function: execute_line:
+* --------------------
+* runs the different functions that will prepare and execute the command line
+* introduced by the user.
+*
+*  line: pointer where will be stored the input introduced by stdin.
+*
+*  returns: -1 if it has failed or 0 if it was executed correctly.
+*/
 int execute_line(char *line){
+    
+    // Allocates memory for the pointers to tokens.
     char **args = malloc (sizeof(char *) * ARGS_SIZE);
+    
+    // Checks if it has been done correctly.
     if (args){
+        
+        // Obteins all arguments in the line.
         parse_args(args,line);
+        
+        // If there is no arguments then skip the execution.
+        if(args[0]){
         check_internal(args);
+        }
+        
+        // liberates the memory for tha arguments.
+        free(args);
+        return 0;
     }
-    free(args);
+    return -1;
 }
 
+/*
+* Function: parse_args:
+* ---------------------
+* Divides the input line into differents segments into tokens that are divided
+* by blank spaces " " and elimintates the comments that are after "#".
+*
+*  line: pointer where will be stored the input introduced by stdin.
+*
+*  returns: -1 if it has failed or 0 if it was executed correctly.
+*/
 int parse_args(char **args, char *line){
-    int i = 0;
-    char * token = malloc(sizeof(char) * COMMAND_LINE_SIZE);
+    
+    // Count for the tokens and pointer to the begining for each token.
+    int ntoken = 0;
+    char * token;
+
     line = strtok(line, "\n");
     token = strtok(line, " ");
-    args [i] = token;
+    args [ntoken] = token;
     while (token != NULL){
-        i++;
+        printf("[parse_args() ->token %i = %s]\n", ntoken, args[ntoken]);
+        ntoken++;
         token = strtok(NULL, " ");
-        args[i] = token;
+        args[ntoken] = token;
     }
-    return i;
+    return ntoken;
 }
 
 int check_internal(char **args){
