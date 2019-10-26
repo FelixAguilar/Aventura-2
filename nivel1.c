97838003b2bb20f02bc2e0c933b2250ad2a68e27
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2019-10-26 15:51:50 +0200

    finalizada el nivel 1

--- nivel1.c
+++ nivel1.c
@@ -8,17 +8,17 @@
 * Date: 
 */
 
-// Libreries.
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
 // Constants.
 #define _POSIX_C_SOURCE 200112L
 #define COMMAND_LINE_SIZE 1024
 #define ARGS_SIZE 64
 #define PROMPT '$'
+
+// Libreries.
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 // Function headers.
 char * read_line(char *line);
@@ -118,36 +118,79 @@ int execute_line(char *line){
 * Divides the input line into differents segments into tokens that are divided
 * by blank spaces " " and elimintates the comments that are after "#".
 *
+*  args: pointer to the pointers for all tokens obteined from the line.
 *  line: pointer where will be stored the input introduced by stdin.
 *
-*  returns: -1 if it has failed or 0 if it was executed correctly.
+*  returns: the number of tokens obteined from line.
 */
 int parse_args(char **args, char *line){
     
-    // Count for the tokens and pointer to the begining for each token.
+    // Count for the tokens and pointer to for each token.
     int ntoken = 0;
     char * token;
 
+    // Checks and cleans the character "\n" at the end of the string line.
     line = strtok(line, "\n");
+
+    // Gets the first token and saves it in args.
     token = strtok(line, " ");
-    args [ntoken] = token;
-    while (token != NULL){
-        printf("[parse_args() ->token %i = %s]\n", ntoken, args[ntoken]);
-        ntoken++;
-        token = strtok(NULL, " ");
-        args[ntoken] = token;
+    args[ntoken] = token;
+
+    // Prints the obteined token (temporal).
+    printf("[parse_args() ->token %i = %s]\n", ntoken, args[ntoken]);
+
+    // Loop until obteining a token that is NULL or a comment.
+    while (args[ntoken]){
+
+        // If there is a token that starts with "#" then there is a comment.
+        if(*(token) == '#'){
+
+            // Stops the search for tokens and adds the sentinel at args.
+            args[ntoken] = NULL;
+
+            // Prints de correction if there is a comment (temporal).
+            printf("[parse_args() ->token %i  corregido = %s]\n", ntoken, args[ntoken]);
+        }
+        else{
+
+            // It obteins the next token and move by 1 the pointer of args.
+            ntoken++;
+            token = strtok(NULL, " ");
+
+            // Saves in args the obteined token.
+            args[ntoken] = token;
+
+            // Prints the obteined token (temporal).
+            printf("[parse_args() ->token %i = %s]\n", ntoken, args[ntoken]);
+        }
     }
     return ntoken;
 }
 
+/*
+* Function: check_internal:
+* -------------------------
+* Checks if the first token is an internal command or not and if it is calls it
+* and returns 1, or if is not an internal then returns 0 and it not calls at a 
+* function.
+*
+*  args: pointer to the pointers for all tokens obteined from the line.
+*
+*  returns: 1 if it is a internal command, else 0.
+*/
 int check_internal(char **args){
+    
+    // Return value.
     int internalCom=0;
+
+    // Internal commands.
     const char cd[] = "cd";
     const char export[]= "export";
     const char source[] = "source";
     const char jobs[] = "jobs";
     const char ex[] = "exit";
     
+    //Checks if it is an internal command, updates return value and calls it.
     if(!strcmp(args[0],cd)){
         internal_cd(args);
         internalCom=1;
@@ -168,30 +211,26 @@ int check_internal(char **args){
         exit(0);
     }
 
+    // returns if it was an internal command.
     return internalCom;
 }
 
 int internal_cd(char **args){
     printf("This function will change the directory.\n");
     return 0;
-
-
 }
 
 int internal_export(char **args){
     printf("This function will asign values to environment variables.\n");
     return 0;
-
 }
 
 int internal_source(char **args){
     printf("This function will execute a command line file.\n");
     return 0;
-
 }
 
 int internal_jobs(char **args){
     printf("This function will show the PID of the processes that are not in the foreground.\n");
     return 0;
-
 }
